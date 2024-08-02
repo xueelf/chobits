1ad8910e63a8b4500beab51952df2194743ebc90
Yuki <admin@yuki.sh>
2024-08-02 17:33:02 +0800

    refractor: initialize project structure

---
+++ scripts/esbuild/build.js
@@ -0,0 +1,22 @@
+import arg from 'arg';
+import { build, context } from 'esbuild';
+import { tscPlugin } from './plugin.js';
+
+const args = arg({
+  '--watch': Boolean,
+  '-w': '--watch',
+});
+/**
+ * @type {import('esbuild').BuildOptions}
+ */
+const options = {
+  bundle: true,
+  format: 'esm',
+  outdir: 'dist',
+  platform: 'node',
+  packages: 'external',
+  plugins: [tscPlugin()],
+  entryPoints: ['src/index.ts'],
+};
+
+args['--watch'] ? await context(options).then(ctx => ctx.watch()) : await build(options);
